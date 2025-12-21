07297efebf319fd7fc504b27f938ebc5f3e85186
M Shubham Achary <shubhamachary56821@gmail.com>
2025-12-21 12:51:43 +0530

    Linear Search

---
+++ LinearSearch/LinearSearch.java
@@ -0,0 +1,31 @@
+package LinearSearch;
+
+class LinearSearch{
+
+    public static int LinearSearch(int[] arr, int target) {
+         for (int i = 0; i < arr.length; i++){
+            if (arr[i] == target){
+                 return i;
+                }
+             }
+             return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
+        int target = 13;
+
+        int result = LinearSearch(arr, target);
+
+        if (result != -1)
+            System.out.println("Target found at index: " + result);
+        else
+            System.out.println("Target not found in the array.");
+    }
+    
+}
+
+
+
+
+
