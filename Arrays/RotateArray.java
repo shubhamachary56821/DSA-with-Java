4fbea1370521045545242a4c2c34c99203da4a75
M Shubham Achary <shubhamachary56821@gmail.com>
2025-12-22 20:32:48 +0530

    Sorted Arrays

---
+++ Arrays/RotateArray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class RotateArray {
+    public static void rotate(int[] arr, int k){
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+    }
+    private static void reverse(int[] arr, int start, int end){
+        while(start < end){
+            int temp= arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+}
