4dca3726bc6eb3d414e3202968178204c8595ea0
M Shubham Achary <shubhamachary56821@gmail.com>
2026-01-23 20:20:16 +0530

    linked list - find middle using slow and fast pointers

--- LinkedList/SinglyLikedList.java
+++ LinkedList/SinglyLikedList.java
@@ -78,5 +78,18 @@ public class SinglyLikedList {
         }
         head = prev;
     }
+
+    public Node findMiddle() {
+        if (head == null) return null;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
 }
 
